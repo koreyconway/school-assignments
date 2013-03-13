f98a4346ca4aa9d794b59a518a68736891e5c038
Korey Conway <korey.conway@gmail.com>
2013-03-13 07:36:17 -0400

    Modified part 1 to make it more modular by breaking things into separate functions Not done fixing it up yet, not yet tested.

--- Assignment 4/Part 1/assign41.c
+++ Assignment 4/Part 1/assign41.c
@@ -2,86 +2,86 @@
 #include <hcs12dp256.h>
 #include <stdio.h>
 
-//char collision_detection (int speed, unsigned int * proximity);
-//void change_speed(int * speed, int value);
+void delay(int ms);
+void keyboard_init(void);
+int  keyboard_get_column(void);
+void keyboard_scan(void);
+void keyboard_pushed(int row, int col);
 
 int main()
 {
-   int row_mask = 0x01;
-   int row = 0;
-   int col = 0;
-   char char_arr[4][4] = {{'1', '2', '3', 'A'},
-   					  {'4', '5', '6', 'B'},
-					  {'7', '8', '9', 'C'},
-					  {'E', '0', 'F', 'D'}};
-   int loopi = 0;
-					
-   
-   setbaud(BAUD19K);
-   
-   // Turn off SPI (not explained why yet)
-   SPI1CR1 = 0x00;
-   
-   // Enable keyboard and disable motors on port P
-   DDRP = 0x0F;
-   
-   // Enable reading from keyboard (High bits of DDRH)
-   DDRH = DDRH & 0x0F;
-   
-   
-   while ( 1 ) {
-   		 PTP = row_mask;
-		 
-		 col = getKeyboardCol();
-		 
-		 // Only output if a button was pushed
-		 if ( PTH & 0xF0 ) {
-		    delay(20);
-			
-			if ( col == getKeyboardCol() ) { 
-					 	putchar(char_arr[row][col]);
-						//printf("Row %d, column %d\n", row_mask, col);
-			}
-			
-			// Check if '0' was pressed, if so then terminate
-			if ( col == 1 && row == 3 ) {
-			   return 1;
-			}
-		 }
-
-		 // Change rows each iteration		 
-		 row++;
-		 row_mask = 0x01 << row;
-
-		 if ( row >= 4 ) {
-		 	row_mask = 0x01;
-			row = 0;
-		 }
-   }
- 
-   return 1;
+	setbaud(BAUD19K);
+	keyboard_init();
+	keyboard_scan();
+	return 1;
 }
 
-int getKeyboardCol()
+void keyboard_scan()
 {
- 	 int col;
-     if ( PTH & 0x10 ) {
+	int row = 0;
+	int col = 0;
+
+	while ( 1 ) {
+		PTP = 0x01 << row;
+
+		col = keyboard_get_column();
+
+		// Only react if a button was pushed
+		if ( PTH & 0xF0 ) {
+			delay(20); // delay for debounce
+
+			if ( col == keyboard_get_column() ) { 
+				keyboard_pushed(row, col);
+				//printf("Row %d, column %d\n", row_mask, col);
+			}
+		}
+
+		// Change rows each iteration		 
+		row = (row+1) % 4;
+	}
+}
+
+void keyboard_pushed(int row, int col)
+{
+	char char_arr[4][4] =
+		{{'1', '2', '3', 'A'},
+		{'4', '5', '6', 'B'},
+		{'7', '8', '9', 'C'},
+		{'E', '0', 'F', 'D'}};
+
+	putchar(char_arr[row][col]);
+	if ( col == 1 && row == 3 ) {
+		asm("SWI");
+	}
+}
+
+void keyboard_init()
+{
+	SPI1CR1 = 0x00; // Turn off SPI (not explained why yet)
+	DDRP = 0x0F; // Enable keyboard and disable motors on port P
+	DDRH = DDRH & 0x0F; // Enable reading from keyboard (High bits of DDRH)
+}
+
+int keyboard_get_column()
+{
+	int col;
+	if ( PTH & 0x10 ) {
 		col = 0;
-	 } else if ( PTH & 0x20 ) {
-	    col = 1;
-	 } else if ( PTH & 0x40 ) {
-	   	col = 2;
-	 } else if ( PTH & 0x80 ) {
-	    col = 3;
-	 }
-	 return col;
+	} else if ( PTH & 0x20 ) {
+		col = 1;
+	} else if ( PTH & 0x40 ) {
+		col = 2;
+	} else if ( PTH & 0x80 ) {
+		col = 3;
+	}
+	return col;
 }
 
 void delay(int ms)
 {
- 	 const int delayConstant = 1234;
-	 int i, j;
-	  for ( i=0; i<ms; i++ ) {
- 	  	  for ( j=0; j < delayConstant; j++ ) {}
-	 }
+	const int delayConstant = 1234;
+	int i, j;
+	for ( i=0; i<ms; i++ ) {
+		for ( j=0; j < delayConstant; j++ ) {}
+	}
 }
--- Assignment 4/Part 1/assign41.s
+++ Assignment 4/Part 1/assign41.s
@@ -1,6 +1,146 @@
 	.module assign41.c
 	.area text
+	.dbfile M:\SYSC20~2\Assignments\ASSIGN~3\PART1~1\assign41.c
+	.dbfunc e main _main fI
+_main::
+	.dbline -1
+	.dbline 16
+; //#include <hc12.h>
+; #include <hcs12dp256.h>
+; #include <stdio.h>
+; 
+; void delay(int ms);
+; void keyboard_init(void);
+; int  keyboard_get_column(void);
+; void keyboard_scan(void);
+; void keyboard_pushed(int row, int col);
+; 
+; //char collision_detection (int speed, unsigned int * proximity);
+; //void change_speed(int * speed, int value);
+; 
+; 
+; int main()
+; {
+	.dbline 17
+; 	setbaud(BAUD19K);
+	ldd #26
+	jsr _setbaud
+	.dbline 18
+; 	keyboard_init();
+	jsr _keyboard_init
+	.dbline 19
+; 	keyboard_scan();
+	jsr _keyboard_scan
+	.dbline 20
+; 	return 1;
+	ldd #1
+	.dbline -2
+L3:
+	.dbline 0 ; func end
+	rts
+	.dbend
+	.dbfunc e keyboard_scan _keyboard_scan fV
+;            col -> -4,x
+;            row -> -2,x
+_keyboard_scan::
+	pshx
+	tfr s,x
+	leas -8,sp
+	.dbline -1
+	.dbline 24
+; }
+; 
+; void keyboard_scan()
+; {
+	.dbline 25
+; 	int row = 0;
+	ldd #0
+	std -2,x
+	.dbline 26
+; 	int col = 0;
+	ldd #0
+	std -4,x
+	bra L6
+L5:
+	.dbline 28
+; 
+; 	while ( 1 ) {
+	.dbline 29
+; 		PTP = 0x01 << row;
+	ldd #1
+	ldy -2,x
+	cpy #0
+	beq X0
+X1:
+	lsld
+	dbne y,X1
+X0:
+	stab 0x258
+	.dbline 31
+; 
+; 		col = keyboard_get_column();
+	jsr _keyboard_get_column
+	std -4,x
+	.dbline 34
+; 
+; 		// Only react if a button was pushed
+; 		if ( PTH & 0xF0 ) {
+	brclr 0x260,#240,L8
+	.dbline 34
+	.dbline 35
+; 			delay(20); // delay for debounce
+	ldd #20
+	jsr _delay
+	.dbline 37
+; 
+; 			if ( col == keyboard_get_column() ) { 
+	jsr _keyboard_get_column
+	std -6,x
+	ldd -4,x
+	cpd -6,x
+	bne L10
+	.dbline 37
+	.dbline 38
+; 				keyboard_pushed(row, col);
+	movw -4,x,0,sp
+	ldd -2,x
+	jsr _keyboard_pushed
+	.dbline 40
+; 				//printf("Row %d, column %d\n", row_mask, col);
+; 			}
+L10:
+	.dbline 41
+; 		}
+L8:
+	.dbline 44
+	ldd -2,x
+	addd #1
+	ldy #4
+	exg x,y
+	idivs
+	exg x,y
+	std -2,x
+	.dbline 45
+L6:
+	.dbline 28
+	bra L5
+X2:
+	.dbline -2
+	.dbline 46
+; 
+; 		// Change rows each iteration		 
+; 		row = (row+1) % 4;
+; 	}
+; }
 L4:
+	tfr x,s
+	pulx
+	.dbline 0 ; func end
+	rts
+	.dbsym l col -4 I
+	.dbsym l row -2 I
+	.dbend
+L13:
 	.byte 49,50
 	.byte 51,'A
 	.byte 52,53
@@ -9,277 +149,169 @@ L4:
 	.byte 57,'C
 	.byte 'E,48
 	.byte 'F,'D
-	.dbfile M:\SYSC20~2\Assignments\ASSIGN~3\PART1~1\assign41.c
-	.dbfunc e main _main fI
-;          loopi -> -24,x
-;       char_arr -> -22,x
-;            col -> -6,x
-;       row_mask -> -4,x
-;            row -> -2,x
-_main::
+	.dbfunc e keyboard_pushed _keyboard_pushed fV
+;       char_arr -> -16,x
+;            col -> 6,x
+;            row -> 2,x
+_keyboard_pushed::
+	pshd
 	pshx
 	tfr s,x
-	leas -30,sp
+	leas -20,sp
 	.dbline -1
-	.dbline 9
-; //#include <hc12.h>
-; #include <hcs12dp256.h>
-; #include <stdio.h>
-; 
-; //char collision_detection (int speed, unsigned int * proximity);
-; //void change_speed(int * speed, int value);
-; 
-; int main()
-; {
-	.dbline 10
-;    int row_mask = 0x01;
-	ldd #1
-	std -4,x
-	.dbline 11
-;    int row = 0;
-	ldd #0
-	std -2,x
-	.dbline 12
-;    int col = 0;
-	ldd #0
-	std -6,x
-	.dbline 13
-;    char char_arr[4][4] = {{'1', '2', '3', 'A'},
-	leay -22,x
+	.dbline 49
+	.dbline 51
+	leay -16,x
 	xgdy
-	ldy #L4
+	ldy #L13
 	pshx
 	tfr d,x
 	ldd #8
-X0:
+X3:
 	movw 2,y+,2,x+
-	dbne d,X0
+	dbne d,X3
 	pulx
-	.dbline 17
-;    					  {'4', '5', '6', 'B'},
-; 					  {'7', '8', '9', 'C'},
-; 					  {'E', '0', 'F', 'D'}};
-;    int loopi = 0;
-	ldd #0
-	std -24,x
-	.dbline 20
-; 					
-;    
-;    setbaud(BAUD19K);
-	ldd #26
-	jsr _setbaud
-	.dbline 23
-;    
-;    // Turn off SPI (not explained why yet)
-;    SPI1CR1 = 0x00;
-	clr 0xf0
-	.dbline 26
-;    
-;    // Enable keyboard and disable motors on port P
-;    DDRP = 0x0F;
-	ldab #15
-	stab 0x25a
-	.dbline 29
-;    
-;    // Enable reading from keyboard (High bits of DDRH)
-;    DDRH = DDRH & 0x0F;
-	bclr 0x262,#0xf0
-	lbra L6
-L5:
-	.dbline 32
-;    
-;    
-;    while ( 1 ) {
-	.dbline 33
-;    		 PTP = row_mask;
-	ldd -4,x
-	stab 0x258
-	.dbline 35
-; 		 
-; 		 col = getKeyboardCol();
-	jsr _getKeyboardCol
-	std -6,x
-	.dbline 38
-; 		 
-; 		 // Only output if a button was pushed
-; 		 if ( PTH & 0xF0 ) {
-	brclr 0x260,#240,L8
-	.dbline 38
-	.dbline 39
-; 		    delay(20);
-	ldd #20
-	jsr _delay
-	.dbline 41
-; 			
-; 			if ( col == getKeyboardCol() ) { 
-	jsr _getKeyboardCol
-	std -26,x
-	ldd -6,x
-	cpd -26,x
-	bne L10
-	.dbline 41
-	.dbline 42
-; 					 	putchar(char_arr[row][col]);
-	ldd -2,x
+	.dbline 56
+	ldd 2,x
 	lsld
 	lsld
-	leay -22,x
-	sty -28,x
-	addd -28,x
-	std -30,x
-	ldd -6,x
-	addd -30,x
+	leay -16,x
+	sty -18,x
+	addd -18,x
+	std -20,x
+	ldd 6,x
+	addd -20,x
 	xgdy
 	ldab 0,y
 	clra
 	jsr _putchar
-	.dbline 44
-; 						//printf("Row %d, column %d\n", row_mask, col);
-; 			}
-L10:
-	.dbline 47
-; 			
-; 			// Check if '0' was pressed, if so then terminate
-; 			if ( col == 1 && row == 3 ) {
-	ldd -6,x
-	cpd #1
-	bne L12
-	ldd -2,x
-	cpd #3
-	bne L12
-	.dbline 47
-	.dbline 48
-; 			   return 1;
-	ldd #1
-	bra L3
-L12:
-	.dbline 50
-; 			}
-; 		 }
-L8:
-	.dbline 53
-; 
-; 		 // Change rows each iteration		 
-; 		 row++;
-	ldd -2,x
-	addd #1
-	std -2,x
-	.dbline 54
-; 		 row_mask = 0x01 << row;
-	ldd #1
-	ldy -2,x
-	cpy #0
-	beq X1
-X2:
-	lsld
-	dbne y,X2
-X1:
-	std -4,x
-	.dbline 56
-; 
-; 		 if ( row >= 4 ) {
-	ldd -2,x
-	cpd #4
-	blt L14
-	.dbline 56
 	.dbline 57
-; 		 	row_mask = 0x01;
-	ldd #1
-	std -4,x
+	ldd 6,x
+	cpd #1
+	bne L14
+	ldd 2,x
+	cpd #3
+	bne L14
+	.dbline 57
 	.dbline 58
-; 			row = 0;
-	ldd #0
-	std -2,x
+		SWI
+
 	.dbline 59
-; 		 }
 L14:
-	.dbline 60
-L6:
-	.dbline 32
-	lbra L5
-X3:
-	.dbline 62
-;    }
-;  
-;    return 1;
-	ldd #1
 	.dbline -2
-L3:
+	.dbline 60
+; 
+; void keyboard_pushed(int row, int col)
+; {
+; 	char char_arr[4][4] =
+; 		{{'1', '2', '3', 'A'},
+; 		{'4', '5', '6', 'B'},
+; 		{'7', '8', '9', 'C'},
+; 		{'E', '0', 'F', 'D'}};
+; 
+; 	putchar(char_arr[row][col]);
+; 	if ( col == 1 && row == 3 ) {
+; 		asm("SWI");
+; 	}
+; }
+L12:
 	tfr x,s
 	pulx
+	leas 2,sp
 	.dbline 0 ; func end
 	rts
-	.dbsym l loopi -24 I
-	.dbsym l char_arr -22 A[16:4:4]c
-	.dbsym l col -6 I
-	.dbsym l row_mask -4 I
-	.dbsym l row -2 I
+	.dbsym l char_arr -16 A[16:4:4]c
+	.dbsym l col 6 I
+	.dbsym l row 2 I
 	.dbend
-	.dbfunc e getKeyboardCol _getKeyboardCol fI
+	.dbfunc e keyboard_init _keyboard_init fV
+_keyboard_init::
+	.dbline -1
+	.dbline 63
+; 
+; void keyboard_init()
+; {
+	.dbline 64
+; 	SPI1CR1 = 0x00; // Turn off SPI (not explained why yet)
+	clr 0xf0
+	.dbline 65
+; 	DDRP = 0x0F; // Enable keyboard and disable motors on port P
+	ldab #15
+	stab 0x25a
+	.dbline 66
+; 	DDRH = DDRH & 0x0F; // Enable reading from keyboard (High bits of DDRH)
+	bclr 0x262,#0xf0
+	.dbline -2
+	.dbline 67
+; }
+L16:
+	.dbline 0 ; func end
+	rts
+	.dbend
+	.dbfunc e keyboard_get_column _keyboard_get_column fI
 ;            col -> -2,x
-_getKeyboardCol::
+_keyboard_get_column::
 	pshx
 	tfr s,x
 	leas -2,sp
 	.dbline -1
-	.dbline 66
-; }
+	.dbline 70
 ; 
-; int getKeyboardCol()
+; int keyboard_get_column()
 ; {
-	.dbline 68
-;  	 int col;
-;      if ( PTH & 0x10 ) {
-	brclr 0x260,#16,L17
-	.dbline 68
-	.dbline 69
+	.dbline 72
+; 	int col;
+; 	if ( PTH & 0x10 ) {
+	brclr 0x260,#16,L18
+	.dbline 72
+	.dbline 73
 ; 		col = 0;
 	ldd #0
 	std -2,x
-	.dbline 70
-	bra L18
-L17:
-	.dbline 70
-; 	 } else if ( PTH & 0x20 ) {
-	brclr 0x260,#32,L19
-	.dbline 70
-	.dbline 71
-; 	    col = 1;
-	ldd #1
-	std -2,x
-	.dbline 72
-	bra L20
-L19:
-	.dbline 72
-; 	 } else if ( PTH & 0x40 ) {
-	brclr 0x260,#64,L21
-	.dbline 72
-	.dbline 73
-; 	   	col = 2;
-	ldd #2
-	std -2,x
 	.dbline 74
-	bra L22
-L21:
+	bra L19
+L18:
 	.dbline 74
-; 	 } else if ( PTH & 0x80 ) {
-	brclr 0x260,#128,L23
+; 	} else if ( PTH & 0x20 ) {
+	brclr 0x260,#32,L20
 	.dbline 74
 	.dbline 75
-; 	    col = 3;
-	ldd #3
+; 		col = 1;
+	ldd #1
 	std -2,x
 	.dbline 76
-; 	 }
-L23:
-L22:
+	bra L21
 L20:
-L18:
+	.dbline 76
+; 	} else if ( PTH & 0x40 ) {
+	brclr 0x260,#64,L22
+	.dbline 76
 	.dbline 77
-; 	 return col;
+; 		col = 2;
+	ldd #2
+	std -2,x
+	.dbline 78
+	bra L23
+L22:
+	.dbline 78
+; 	} else if ( PTH & 0x80 ) {
+	brclr 0x260,#128,L24
+	.dbline 78
+	.dbline 79
+; 		col = 3;
+	ldd #3
+	std -2,x
+	.dbline 80
+; 	}
+L24:
+L23:
+L21:
+L19:
+	.dbline 81
+; 	return col;
 	ldd -2,x
 	.dbline -2
-L16:
+L17:
 	tfr x,s
 	pulx
 	.dbline 0 ; func end
@@ -297,57 +329,57 @@ _delay::
 	tfr s,x
 	leas -6,sp
 	.dbline -1
-	.dbline 81
+	.dbline 85
 ; }
 ; 
 ; void delay(int ms)
 ; {
-	.dbline 82
-;  	 const int delayConstant = 1234;
+	.dbline 86
+; 	const int delayConstant = 1234;
 	ldd #1234
 	std -4,x
-	.dbline 84
+	.dbline 88
 	ldd #0
 	std -6,x
-	bra L29
-L26:
-	.dbline 84
-; 	 int i, j;
-; 	  for ( i=0; i<ms; i++ ) {
-	.dbline 85
+	bra L30
+L27:
+	.dbline 88
+; 	int i, j;
+; 	for ( i=0; i<ms; i++ ) {
+	.dbline 89
 	ldd #0
 	std -2,x
-	bra L33
-L30:
-	.dbline 85
-	.dbline 85
+	bra L34
 L31:
-	.dbline 85
+	.dbline 89
+	.dbline 89
+L32:
+	.dbline 89
 	ldd -2,x
 	addd #1
 	std -2,x
-L33:
-	.dbline 85
+L34:
+	.dbline 89
 	ldd -2,x
 	cpd -4,x
-	blt L30
-	.dbline 86
-L27:
-	.dbline 84
+	blt L31
+	.dbline 90
+L28:
+	.dbline 88
 	ldd -6,x
 	addd #1
 	std -6,x
-L29:
-	.dbline 84
+L30:
+	.dbline 88
 	ldd -6,x
 	cpd 2,x
-	blt L26
+	blt L27
 	.dbline -2
-	.dbline 87
-;  	  	  for ( j=0; j < delayConstant; j++ ) {}
-; 	 }
+	.dbline 91
+; 		for ( j=0; j < delayConstant; j++ ) {}
+; 	}
 ; }
-L25:
+L26:
 	tfr x,s
 	pulx
 	leas 2,sp
